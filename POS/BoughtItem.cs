519b0c26e3c630c9789f4719f6ebf4bf3c4f99e4
shafikshaon <shafikshaon@gmail.com>
2017-12-23 11:53:27 +0600

    Item, Bought Item model added

---
+++ POS/BoughtItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POS
+{
+    class BoughtItem
+    {
+        public Item Item { get; set; }
+        public int Quantity { get; set; }
+    }
+}
---
+++ POS/Item.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POS
+{
+    class Item
+    {
+        public int Id { get; set; }
+        public string ItemName { get; set; }
+        public int ItemPrice { get; set; }
+        public int ItemStock { get; set; }
+    }
+}
